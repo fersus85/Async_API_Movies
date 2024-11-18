2a99d8965202005cebb620c98fce91c8ef13da67
dbacvetkov <dmitry.cvetkov@gmail.com>
2024-11-18 14:19:38 +0000

    Исправление замечаний

--- tests/functional/src/test_film.py
+++ tests/functional/src/test_film.py
@@ -1,10 +1,13 @@
 import pickle
-from typing import Dict
+from typing import Any, Callable, Dict
+from urllib.parse import urlencode
 from uuid import uuid4
 
 import pytest
+from aiohttp import ClientResponse
 from db.redis import form_key
 from models.film import Film
+from redis.asyncio import Redis
 from tests.functional.settings import test_settings
 
 
@@ -27,13 +30,30 @@ from tests.functional.settings import test_settings
             "INCORRECT-UUID-FORMAT",
             {"status": 404, "title": None, "imdb_rating": None},
         ),
+        (
+            "/",
+            {"status": 404, "title": None, "imdb_rating": None},
+        ),
     ],
 )
 @pytest.mark.asyncio
-async def test_search_film_by_id(
-    make_get_request, query_data: str, exp_answer: Dict
+async def test_get_film_by_id(
+    make_get_request: Callable[[str, str], ClientResponse],
+    query_data: str,
+    exp_answer: Dict[str, Any],
 ):
+    """
+    Тестирует получение информации о фильме по его ID.
 
+    Проверяет следующие сценарии:
+    - Успешный запрос с корректным ID.
+    - Запрос с несуществующим ID, который возвращает статус NOT_FOUND.
+    - Запрос с некорректным форматом ID, который возвращает статус NOT_FOUND.
+
+    :param make_get_request: Фикстура для выполнения GET-запроса.
+    :param query_data: ID для запроса.
+    :param exp_answer: Ожидаемый ответ, содержащий статус и название фильма.
+    """
     response = await make_get_request(test_settings.ES_FILM_IDX, query_data)
 
     body = await response.json()
@@ -45,8 +65,14 @@ async def test_search_film_by_id(
 
 
 @pytest.mark.asyncio
-async def test_list_films(make_get_request):
+async def test_list_films(
+    make_get_request: Callable[[str, str], ClientResponse]
+):
+    """
+    Тестирует получение всех загруженных для теста фильмов.
 
+    :param make_get_request: Фикстура для выполнения GET-запроса.
+    """
     response = await make_get_request(test_settings.ES_FILM_IDX, "")
 
     body = await response.json()
@@ -79,17 +105,30 @@ async def test_list_films(make_get_request):
             {"page_size": 51, "page_number": 1},
             {"status": 422, "body_len": 1},
         ),
+        (
+            {"page_size": 0, "page_number": 0},
+            {"status": 422, "body_len": 1},
+        ),
+        (
+            {"page_size": -1, "page_number": -1},
+            {"status": 422, "body_len": 1},
+        ),
     ],
 )
 @pytest.mark.asyncio
 async def test_list_films_with_pages(
-    make_get_request, query_data: Dict, exp_answer: Dict
+    make_get_request: Callable[[str, str], ClientResponse],
+    query_data: Dict[str, Any],
+    exp_answer: Dict[str, Any],
 ):
+    """
+    Тестирует пагинацию при получении списка фильмов.
 
-    page_size = query_data["page_size"]
-    page_number = query_data["page_number"]
-
-    query_parameters = f"?page_size={page_size}&page_number={page_number}"
+    Параметры:
+    - query_data (Dict): Параметры запроса для пагинации.
+    - exp_answer (Dict): Ожидаемый ответ, со статусом и длиной списка фильмов.
+    """
+    query_parameters = f"?{urlencode(query_data)}"
 
     response = await make_get_request(
         test_settings.ES_FILM_IDX, query_parameters
@@ -107,8 +146,8 @@ async def test_list_films_with_pages(
     [
         (
             {
-                "sort_order": "-imdb_rating",
-                "genre_filter": "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff",
+                "sort": "-imdb_rating",
+                "genre_id": "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff",
             },
             {
                 "status": 200,
@@ -118,8 +157,8 @@ async def test_list_films_with_pages(
         ),
         (
             {
-                "sort_order": "imdb_rating",
-                "genre_filter": "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff",
+                "sort": "imdb_rating",
+                "genre_id": "3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff",
             },
             {
                 "status": 200,
@@ -129,8 +168,8 @@ async def test_list_films_with_pages(
         ),
         (
             {
-                "sort_order": "-imdb_rating",
-                "genre_filter": "5373d043-3f41-4ea8-9947-4b746c601bbd",
+                "sort": "-imdb_rating",
+                "genre_id": "5373d043-3f41-4ea8-9947-4b746c601bbd",
             },
             {
                 "status": 200,
@@ -140,8 +179,8 @@ async def test_list_films_with_pages(
         ),
         (
             {
-                "sort_order": "imdb_rating",
-                "genre_filter": "5373d043-3f41-4ea8-9947-4b746c601bbd",
+                "sort": "imdb_rating",
+                "genre_id": "5373d043-3f41-4ea8-9947-4b746c601bbd",
             },
             {
                 "status": 200,
@@ -153,13 +192,19 @@ async def test_list_films_with_pages(
 )
 @pytest.mark.asyncio
 async def test_list_films_with_sort_and_filter(
-    make_get_request, query_data: Dict, exp_answer: Dict
+    make_get_request: Callable[[str, str], ClientResponse],
+    query_data: Dict[str, Any],
+    exp_answer: Dict[str, Any],
 ):
+    """
+    Тестирует сортировку и фильтрацию при получении списка фильмов.
 
-    sort_order = query_data["sort_order"]
-    genre_filter = query_data["genre_filter"]
-
-    query_parameters = f"?sort={sort_order}&genre_id={genre_filter}"
+    Параметры:
+    - query_data (Dict): Параметры сортировки и фильтрации по жанру
+    - exp_answer (Dict): Ожидаемый ответ, со статусом, длиной списка фильмов
+        и первым фильмом в выдаче
+    """
+    query_parameters = f"?{urlencode(query_data)}"
 
     response = await make_get_request(
         test_settings.ES_FILM_IDX, query_parameters
@@ -175,8 +220,19 @@ async def test_list_films_with_sort_and_filter(
 
 
 @pytest.mark.asyncio
-async def test_cache_film_by_id(redis_client, make_get_request):
+async def test_cache_film_by_id(
+    make_get_request: Callable[[str, str], ClientResponse], redis_client: Redis
+):
+    """
+    Тестирует кэширование при получении списка фильмов по его id.
 
+    Параметры:
+    - make_get_request: Фикстура для выполнения запроса к API.
+    - redis_client: Фикстура Redis для работы с кэшем.
+
+    Проверяет, что данные кэша возвращаются корректно,
+    и что кэш очищается после запроса.
+    """
     film_id = str(uuid4())
     key = form_key("get_by_id", (film_id,), {})
     test_film_data = Film(
