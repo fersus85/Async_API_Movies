007dc5600793c426d28690980549219230ac8b4b
deriabin_85 <deriabin_85@mail.ru>
2024-11-26 13:04:22 +0300

    Добавил тест на кэширование get_person_by_id

--- tests/functional/src/test_person.py
+++ tests/functional/src/test_person.py
@@ -1,5 +1,11 @@
-import pytest
 from typing import Dict
+from uuid import uuid4
+import pickle
+import pytest
+
+from db.redis import form_key
+from models.person import Person
+from tests.functional.settings import test_settings
 
 
 @pytest.mark.parametrize(
@@ -13,6 +19,10 @@ from typing import Dict
             "a5a8f513-3c7e-4vcc-8a2b-91cbff55250a",
             {"status": 404, "name": None},
         ),
+        (
+            "",
+            {"status": 404, "name": None},
+        ),
     ],
 )
 @pytest.mark.asyncio
@@ -20,10 +30,31 @@ async def test_search_person_by_id(
     make_get_request, query_data: str, exp_answer: Dict
 ):
 
-    response = await make_get_request(query_data)
+    response = await make_get_request(test_settings.ES_PERSON_IDX, query_data)
 
     body = await response.json()
     status = response.status
 
     assert status == exp_answer.get("status")
     assert body.get("full_name") == exp_answer.get("name")
+
+
+@pytest.mark.asyncio
+async def test_cache_person_by_id(redis_client, make_get_request):
+    person_id = str(uuid4())
+    key = form_key("get_by_id", (person_id,), {})
+    test_person_data = Person(id=person_id, full_name="CACHE", films=[])
+
+    await redis_client.set(key, pickle.dumps(test_person_data), ex=60)
+
+    response = await make_get_request(test_settings.ES_PERSON_IDX, person_id)
+    body = await response.json()
+
+    assert response.status == 200
+    assert body["uuid"] == person_id
+    assert body["full_name"] == "CACHE"
+
+    await redis_client.delete(key)
+
+    response = await make_get_request(test_settings.ES_PERSON_IDX, person_id)
+    assert response.status == 404
